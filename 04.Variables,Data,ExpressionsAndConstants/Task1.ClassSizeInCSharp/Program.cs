215c41d0f370ad7bd6f3c90f44100bad4a8fd562
danisio <dany_s@abv.bg>
2015-07-01 15:22:19 +0300

    Variables,Data,ExpressionsAndConstants

---
+++ 04.Variables,Data,ExpressionsAndConstants/Task1.ClassSizeInCSharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Size
+{
+    public Size(double width, double height)
+    {
+        this.Width = width;
+        this.Height = height;
+    }
+
+    public double Width { get; set; }
+
+    public double Height { get; set; }
+
+    public static Size GetRotatedSize(Size size, double angleOfTheFigureThatWillBeRotated)
+    {
+        double newWidth = (Math.Abs(Math.Cos(angleOfTheFigureThatWillBeRotated)) * size.Width) +
+                          (Math.Abs(Math.Sin(angleOfTheFigureThatWillBeRotated)) * size.Height);
+
+        double newHeight = (Math.Abs(Math.Sin(angleOfTheFigureThatWillBeRotated)) * size.Width) +
+                           (Math.Abs(Math.Cos(angleOfTheFigureThatWillBeRotated)) * size.Height);
+
+        return new Size(newWidth, newHeight);
+    }
+}
---
+++ 04.Variables,Data,ExpressionsAndConstants/Task2.MethodPrintStatisticsInCSharp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Statistics
+{
+    public static void PrintStatistics(double[] arr, int count)
+    {
+        double max = double.MinValue;
+        for (int i = 0; i < count; i++)
+        {
+            if (arr[i] > max)
+            {
+                max = arr[i];
+            }
+        }
+
+        Console.WriteLine("The MAX value is: " + max);
+
+        double min = double.MaxValue;
+        for (int i = 0; i < count; i++)
+        {
+            if (arr[i] < min)
+            {
+                min = arr[i];
+            }
+        }
+
+        Console.WriteLine("The MIN value is: " + min);
+
+        double sum = 0;
+        for (int i = 0; i < count; i++)
+        {
+            sum += arr[i];
+        }
+
+        Console.WriteLine("The AVERAGE value is: " + (sum / count));
+    }
+}
